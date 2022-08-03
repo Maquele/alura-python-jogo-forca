269fd806138374434b81c23f07bb692ee435059f
Maquele <maquele_antunes@yahoo.com>
2022-08-03 17:02:47 -0300

    first commit

---
+++ forca.py
@@ -0,0 +1,50 @@
+import random
+
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da força!***")
+    print("*********************************")
+
+    palavras = []
+    with open("palavras.txt", "r") as arquivo:
+        for linha in arquivo:
+            linha = linha.strip()
+            palavras.append(linha)
+    
+    numero = random.randrange(0, len(palavras))
+
+    palavra_secreta = palavras[numero].strip().lower()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
+    enforcou = False
+    acertou = False
+    tentativas = 0
+
+    print(letras_acertadas)
+
+    while(not enforcou and not acertou):
+        chute = input("Qual letra? ")
+        chute = chute.strip().lower()
+        print(f"Rodada {tentativas}")
+    
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[index] = chute
+                    print(f"Acertou a letra: {chute} na posição {index}")
+                index = index + 1
+        else:
+            print("Você errou :(")
+            tentativas = tentativas + 1
+        enforcou = tentativas == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+    
+    if (acertou):
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
+
+if(__name__ == "__main__"):
+    jogar()
